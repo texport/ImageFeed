d2869831680a8f8374588e02f1b5a018f3d85854
Sergey Ivanov <s.ivanov@iberica.kz>
2024-02-19 19:56:18 +0600

    Доработка приложения согласно заданию 8-го спринта

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,90 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Sergey Ivanov on 19.02.2024.
+//
+
+import UIKit
+
+@IBDesignable
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    private let container = UIView()
+    
+    let customImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    let customLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.text = "31 августа 2024"
+        return label
+    }()
+
+    let customButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "Active"), for: .normal)
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        
+    private func setupViews() {
+        contentView.addSubview(container)
+        container.translatesAutoresizingMaskIntoConstraints = false
+        
+        // констреинты контейнера
+        NSLayoutConstraint.activate([
+            container.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8), // Верхний отступ
+            container.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8), // Нижний отступ
+            container.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16), // Левый отступ
+            container.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16), // Правый отступ
+        ])
+        
+        container.addSubview(customImageView)
+        container.addSubview(customLabel)
+        container.addSubview(customButton)
+        
+        NSLayoutConstraint.activate([
+            customImageView.topAnchor.constraint(equalTo: container.topAnchor),
+            customImageView.bottomAnchor.constraint(equalTo: container.bottomAnchor),
+            customImageView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            customImageView.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+            
+            customLabel.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 8),
+            customLabel.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -16),
+            
+            customButton.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+            customButton.topAnchor.constraint(equalTo: container.topAnchor),
+            customButton.widthAnchor.constraint(equalToConstant: 44), // Ширина кнопки
+            customButton.heightAnchor.constraint(equalToConstant: 44), // Высота кнопки
+        ])
+        
+        // Скругление углов ячейки
+        container.layer.cornerRadius = 16
+        container.clipsToBounds = true
+        
+        customImageView.layer.cornerRadius = 16
+        customImageView.clipsToBounds = true
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.backgroundColor = UIColor(named: "YP Black")
+    }
+}
