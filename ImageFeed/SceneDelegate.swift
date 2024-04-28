bdd47bd1292762693e770ab49e9a54fc5e5be62c
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2024-04-28 18:55:22 +0500

    no message

--- ImageFeed/SceneDelegate.swift
+++ ImageFeed/SceneDelegate.swift
@@ -10,13 +10,12 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
         
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = SplashViewController() // Изменено здесь
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = SplashViewController()
         window?.makeKeyAndVisible()
     }
 
